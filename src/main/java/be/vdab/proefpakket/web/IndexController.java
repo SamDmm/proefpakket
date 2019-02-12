8ed6ac990b52410aac16cc31614bbffb8e0b1d9d
Sam Demeulemeester <sam.demeulemeester@vdabcampus.be>
2019-02-12 15:45:27 +0100

    Brouwers

--- src/main/java/be/vdab/proefpakket/web/IndexController.java
+++ src/main/java/be/vdab/proefpakket/web/IndexController.java
@@ -4,11 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import be.vdab.proefpakket.services.BrouwerService;
+
 @Controller
 @RequestMapping("/")
 class IndexController {
 	private static final String VIEW = "index";
+	private static final char[] ALFABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+	private final BrouwerService brouwerService;
+	
+	IndexController(BrouwerService brouwerService) {
+		this.brouwerService = brouwerService;
+	}
+
 	ModelAndView index() {
-		return new ModelAndView(VIEW);
+		return new ModelAndView(VIEW, "alfabet", ALFABET);
 	}
 }
